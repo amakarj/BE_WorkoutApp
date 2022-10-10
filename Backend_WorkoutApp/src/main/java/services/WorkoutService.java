387c58755cfab805f4232ee2d146fcc5d947adcd
Jenna Hakkarainen <75015030+jenhakk@users.noreply.github.com>
2022-10-10 19:36:09 +0300

    Update WorkoutService.java

--- Backend_WorkoutApp/src/main/java/services/WorkoutService.java
+++ Backend_WorkoutApp/src/main/java/services/WorkoutService.java
@@ -1,6 +1,5 @@
 package services;
 
-import java.awt.List;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,6 +9,7 @@ import java.util.ArrayList;
 
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -387,7 +387,7 @@ public String hello() {
 	public ArrayList<Exercise> setCheckedToFalse(ArrayList<Exercise> list) {
 		
 		Connection conn = Connections.getConnection();
-		System.out.println(list);
+		//System.out.println(list);
 		int i = 0;
 	
 
@@ -402,7 +402,7 @@ public String hello() {
 				pstmt.setBoolean(1, false);
 				pstmt.setInt(2, exercise.getExerciseid());
 				pstmt.executeUpdate();
-				System.out.println(exercise.getChecked());
+				//System.out.println(exercise.getChecked());
 				i++;
 			
 
@@ -444,7 +444,44 @@ public String hello() {
 				workout.setPersonid(RS.getInt("personid"));
 
 				list.add(workout);
-				System.out.println("onko se tämä " + list.toString());
+				//System.out.println("onko se tämä " + list.toString());
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return list;
+
+	}
+	
+	@GET
+	@Path("/readallworkouts")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Workout> readAllWorkouts() {
+
+		ArrayList<Workout> list = new ArrayList<>();
+		Connection conn = Connections.getConnection();
+		try {
+			String sql = "select * from workout order by workoutid desc";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet RS = stmt.executeQuery();
+
+			while (RS.next()) {
+				Workout workout = new Workout();
+				workout.setWorkoutid(RS.getInt("workoutid"));
+				workout.setDate(RS.getString("date"));
+				workout.setPersonid(RS.getInt("personid"));
+
+				list.add(workout);
+				//System.out.println("onko se tämä " + list.toString());
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -481,7 +518,7 @@ public String hello() {
 				workout.setPersonid(RS.getInt("personid"));
 
 				list.add(workout);
-				System.out.println(list.toString());
+				//System.out.println(list.toString());
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -569,6 +606,33 @@ public String hello() {
 		return list1;
 
 	}
+	
+	@DELETE
+	@Path("/deleteworkoutbyid/{workoutid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Workout> deleteWorkout(@PathParam("workoutid") int workoutid) {
+		System.out.println("pääseekö deleteen?" + workoutid);
+		Connection conn=Connections.getConnection();
+		try {
+			PreparedStatement pstmt=conn.prepareStatement("delete from workout where id=?");
+			pstmt.setInt(1, workoutid);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		ArrayList<Workout> list=readAllWorkouts();		
+		return list;
+	}	
+	
 
 	// WORKOUTEXERCISE SERVICES
 
@@ -728,7 +792,7 @@ public String hello() {
 			}
 		}
 		
-		System.out.println(separatedList);
+		//System.out.println(separatedList);
 		
 
 		
